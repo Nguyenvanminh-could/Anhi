c13df428fe1869f31936da1f9f827ee087cde043
Nguyenvanminh-could <Nguyenminh03102022@gmail.com>
2025-03-13 19:37:40 +0700

    Add files via upload

---
+++ present.js
@@ -0,0 +1,44 @@
+
+var to = 'Gửi Ánh Nhi!';
+var gift_url = 'https://formybae.glitch.me/';
+var gift_image_url = 'Nhi.jpg';
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+  
+  var _giftLink, 
+      _giftImg;
+  
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+  
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+    
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+    document.getElementById("card").classList.add("card_show");
+  }, false);
+  
+  
+  
+  nametag.innerText = to;
+}
+
+init();
+
